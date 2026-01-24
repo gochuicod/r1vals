8b6d15b8ae9ab38031df284608119f1c49adf68e
Darelle <gochuicodarelleq@gmail.com>
2026-01-24 22:48:41 +0800

    feature(navbar): added navbar
    navbar is now reponsive across all screens

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,7 +1,40 @@
-export default function Header() {
+import Image from 'next/image';
+import { cn } from '@/lib/utils';
+import { Link } from '@/i18n/routing';
+
+export default function Navbar() {
   return (
-    <div>
-      <span>this is the header</span>
+    <div
+      className={cn(
+        'fixed top-0 left-0 w-full z-50',
+        'flex flex-row',
+        'justify-between',
+        'items-center',
+        'px-[80px] py-[20px]',
+        'bg-black/35',
+        'max-h-[75px]',
+      )}
+    >
+      {/* Logo here */}
+      <Image src="/r1vals_logo.svg" alt="R1Vals Logo" width={113} height={37} />
+
+      {/* Links here */}
+      <div
+        className={cn(
+          'lg:flex hidden',
+          'flex-row',
+          'text-[#E8F5E8] text-[11px]',
+          'gap-6',
+          'uppercase',
+          'leading-[16px]',
+        )}
+      >
+        <Link href="/#wild-events">wild events</Link>
+        <Link href="/#legends">legends</Link>
+        <Link href="/#chaos-league">chaos league</Link>
+        <Link href="/watch-live">watch live</Link>
+        {/* Register Now Button here */}
+      </div>
     </div>
   );
 }

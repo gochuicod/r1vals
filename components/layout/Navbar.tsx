4fc7f58d8148254be941778be186f1077dac5386
Dolly-Chaerry <douleiamaeyap@gmail.com>
2026-01-27 17:21:54 +0800

    style: yellow #FCC800

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -49,9 +49,9 @@ export default function Navbar() {
               href={link.href}
               onClick={() => setActiveHash(link.hash)}
               className={cn(
-                'transition-all duration-200 hover:text-yellow-400',
+                'transition-all duration-200 hover:text-[#FCC800]',
                 isActive
-                  ? 'text-yellow-400 underline underline-offset-4 decoration-yellow-400'
+                  ? 'text-[#FCC800] underline underline-offset-4 decoration-[#FCC800]'
                   : 'text-inherit',
               )}
             >

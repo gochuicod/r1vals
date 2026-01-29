a1743ae9dcc64f73e73edd135a3e463bc02eda02
Dolly-Chaerry <douleiamaeyap@gmail.com>
2026-01-29 13:06:42 +0800

    feat(navbar): phone view of menu panel

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -6,13 +6,13 @@ import { cn } from '@/lib/utils';
 import { Link } from '@/i18n/routing';
 import { Button } from '@/components/ui/Button';
 import { useSmoothScroll } from '@/hooks/useSmoothScroll';
-import { MenuButton, MenuButtonRef } from '@/components/ui/MenuButton';
-import { Menu } from 'lucide-react';
+import { MenuDropDown, MenuButton, MenuButtonRef } from '../ui/MenuUI';
+import { set } from 'zod';
 
 export default function Navbar() {
   const [activeHash, setActiveHash] = React.useState('');
   const [openMenu, setOpenMenu] = React.useState(false);
-  const menuButtonRef = React.useRef<MenuButtonRef>(null);
+  const menuBtnRef = React.useRef<MenuButtonRef>(null);
   const scrollTo = useSmoothScroll();
 
   React.useEffect(() => {
@@ -39,53 +39,110 @@ export default function Navbar() {
   ];
 
   return (
-    <div className="fixed top-0 left-0 w-full z-[1000] flex justify-between items-center py-4 px-6 lg:px-[80px] lg:py-[20px] bg-black/60 max-h-[75px] backdrop-blur-xl">
-      <Link
-        href="/"
-        onClick={(e) => {
-          setActiveHash('');
-          scrollTo(e, '');
-        }}
-      >
-        <Image
-          src="/r1vals_logo.svg"
-          alt="R1Vals Logo"
-          width={113}
-          height={37}
-          priority
-        />
-      </Link>
-
-      <div className="lg:flex hidden flex-row items-center text-[#E8F5E8] text-[11px] gap-6 uppercase leading-[16px]">
-        {navlinks.map((link) => {
-          const isActive = activeHash !== '' && link.href.includes(activeHash);
-          return (
-            <Link
-              key={link.href}
-              href={link.href}
-              onClick={(e) => {
-                setActiveHash(link.hash); // Keep updating active state
-                scrollTo(e, link.href); // Smooth scroll
-              }}
-              className={cn(
-                'transition-all duration-200 hover:text-[#FCC800]',
-                isActive
-                  ? 'text-[#FCC800] underline underline-offset-4 decoration-[#FCC800]'
-                  : 'text-inherit',
-              )}
+    <>
+      <div className="fixed top-0 left-0 w-full z-[1000] py-4 px-6 lg:px-[80px] lg:py-[20px] bg-black/60 backdrop-blur-xl">
+        <div className="flex justify-between items-center">
+          <Link
+            href="/"
+            onClick={(e) => {
+              setActiveHash('');
+              scrollTo(e, '');
+            }}
+          >
+            <Image
+              src="/r1vals_logo.svg"
+              alt="R1Vals Logo"
+              width={113}
+              height={37}
+              priority
+            />
+          </Link>
+          <MenuButton
+            ref={menuBtnRef}
+            open={openMenu}
+            onToggle={() => setOpenMenu((v) => !v)}
+          />
+          <div className="lg:flex hidden flex-row items-center text-[#E8F5E8] text-[11px] gap-6 uppercase leading-[16px]">
+            {navlinks.map((link) => {
+              const isActive =
+                activeHash !== '' && link.href.includes(activeHash);
+              return (
+                <Link
+                  key={link.href}
+                  href={link.href}
+                  onClick={(e) => {
+                    setActiveHash(link.hash); // Keep updating active state
+                    scrollTo(e, link.href); // Smooth scroll
+                  }}
+                  className={cn(
+                    'transition-all duration-200 hover:text-[#FCC800]',
+                    isActive
+                      ? 'text-[#FCC800] underline underline-offset-4 decoration-[#FCC800]'
+                      : 'text-inherit',
+                  )}
+                >
+                  {link.label}
+                </Link>
+              );
+            })}
+            <Button
+              href="#contact"
+              variant="yellow"
+              size="lg"
+              smoothScroll={true}
             >
-              {link.label}
-            </Link>
-          );
-        })}
-        <Button href="#contact" variant="yellow" size="lg" smoothScroll={true}>
-          Register Now!
-        </Button>
+              Register Now!
+            </Button>
+          </div>
+        </div>
+        <MenuDropDown
+          className="text-white flex flex-col capitalize gap-4 mt-4"
+          open={openMenu}
+          onClose={() => {
+            setOpenMenu(false);
+            menuBtnRef.current?.close();
+          }}
+        >
+          {navlinks.map((link) => {
+            const isActive =
+              activeHash !== '' && link.href.includes(activeHash);
+            return (
+              <Link
+                key={link.href}
+                href={link.href}
+                onClick={(e) => {
+                  setActiveHash(link.hash); // Keep updating active state
+                  scrollTo(e, link.href); // Smooth scroll
+                  setOpenMenu(false);
+                  menuBtnRef.current?.close();
+                }}
+                className={cn(
+                  'transition-all duration-200 hover:text-[#FCC800]',
+                  isActive
+                    ? 'text-[#FCC800] underline underline-offset-4 decoration-[#FCC800]'
+                    : 'text-inherit',
+                )}
+              >
+                {link.label}
+              </Link>
+            );
+          })}
+          <div>
+            <Button
+              href="#contact"
+              variant="yellow"
+              size="sm"
+              smoothScroll={true}
+              onClick={() => {
+                setOpenMenu(false);
+                menuBtnRef.current?.close();
+              }}
+            >
+              Register Now!
+            </Button>
+          </div>
+        </MenuDropDown>
       </div>
-
-      <div className="lg:hidden block">
-        <MenuButton ref={menuButtonRef} className="text-white" />
-      </div>
-    </div>
+    </>
   );
 }
